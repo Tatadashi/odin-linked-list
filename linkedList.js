838e961870153232d9ba76c2de7d3766ba5261f5
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2024-12-31 03:15:50 -0800

    Add prepend method to linkedList

--- linkedList.js
+++ linkedList.js
@@ -1,29 +1,35 @@
 class LinkedList {
-    constructor(head = null) {
-        this.head = head;
+  constructor(head = null) {
+    this.head = head;
+  }
+  append(value) {
+    if (this.head == null) {
+      this.head = new Node(value);
+    } else {
+      let temp = this.head;
+      while (temp.nextNode !== null) {
+        temp = temp.nextNode;
+      }
+      temp.nextNode = new Node(value);
     }
-    append(value) {
-        if (this.head == null) {
-            this.head = new Node(value);
-        } else {
-            let temp = this.head;
-            while(temp.nextNode !== null) {
-                temp = temp.nextNode;
-            }
-            temp.nextNode = new Node(value);
-        }
-    }
+  }
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.nextNode = this.head;
+    this.head = newNode;
+  }
 }
 
 class Node {
-    constructor(value = null, nextNode = null) {
-        this.value = value;
-        this.nextNode = nextNode;
-    }
+  constructor(value = null, nextNode = null) {
+    this.value = value;
+    this.nextNode = nextNode;
+  }
 }
 
 const list = new LinkedList();
-list.append('Jake');
-list.append('Horm');
-list.append('Fac');
-console.log(list.head.nextNode);
+list.append("Jake");
+list.append("Horm");
+list.append("Fac");
+list.prepend("Man");
+console.log(list);
