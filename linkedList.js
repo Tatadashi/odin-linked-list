d91383ab3ceff1b7b22004741f218b9cc649d033
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2024-12-31 02:26:25 -0800

    Create basic 2 classes
    linkedList has no constructor but will have methods

---
+++ linkedList.js
@@ -0,0 +1,14 @@
+class LinkedList {
+
+}
+
+class Node {
+    constructor(value = null, nextNode = null) {
+        this.value = value;
+        this.nextNode = nextNode;
+    }
+}
+
+const list = new LinkedList();
+const nod = new Node();
+console.log(nod);
