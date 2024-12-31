f0d2cf403f6745073a22736afc28960001e686c4
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2024-12-31 02:47:05 -0800

    Add head constructor and appendNode method

--- linkedList.js
+++ linkedList.js
@@ -1,5 +1,18 @@
 class LinkedList {
-
+    constructor(head = null) {
+        this.head = head;
+    }
+    append(value) {
+        if (this.head == null) {
+            this.head = new Node(value);
+        } else {
+            let temp = this.head;
+            while(temp.nextNode !== null) {
+                temp = temp.nextNode;
+            }
+            temp.nextNode = new Node(value);
+        }
+    }
 }
 
 class Node {
@@ -10,5 +23,7 @@ class Node {
 }
 
 const list = new LinkedList();
-const nod = new Node();
-console.log(nod);
+list.append('Jake');
+list.append('Horm');
+list.append('Fac');
+console.log(list.head.nextNode);
