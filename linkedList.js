0ac3555133e4524677127fe888e431b711100412
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2024-12-31 04:22:02 -0800

    Create get for size and tail
    Although could have been methods, makes more sense to me that they are getters since they "get" a "property" of the linked list rather than modifying it. Also the fact that they "feel" like "properties" of linked list similar to how it is Array.length not Array.length() (it would be list.size rather than list.size())

--- linkedList.js
+++ linkedList.js
@@ -5,8 +5,29 @@ class LinkedList {
     this._head = head;
   }
 
+  get size() {
+    let size = 0;
+    if (this._head == null) {
+        return size;
+    } else {
+        size = 1;
+        let temp = this._head;
+        while (temp.nextNode !== null) {
+          temp = temp.nextNode;
+          size++;
+        }
+        return size;
+    }
+  }
   get head() {
     return this._head;
+  }
+  get tail() {
+    let temp = this._head;
+    while (temp.nextNode !== null) {
+      temp = temp.nextNode;
+    }
+    return temp;
   }
 
   append(value) {
@@ -39,4 +60,4 @@ list.append("Jake");
 list.append("Horm");
 list.append("Fac");
 list.prepend("Man");
-console.log(list.head);
+console.log(list.size);
